cd71d6855620d85ff0724d9f7d008accffce5fda
Josh Gearheart <other.josh.gearheart@gmail.com>
2019-07-19 20:50:14 -0700

    begin basic skeleton of site, add styles

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import './styles/App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <p>I am Jonah's website.</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles/Header.scss'
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-item">
+        
+      </div>
+      <div className="header-item">
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
